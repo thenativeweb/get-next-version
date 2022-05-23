90ca16a0c7d97c72449118b76caf0949276ac1e7
Golo Roden <golo.roden@thenativeweb.io>
2022-05-23 12:55:01 +0200

    chore: Use EqualValues instead of Equal. (#10)

--- versioning/next_version_test.go
+++ versioning/next_version_test.go
@@ -83,6 +83,6 @@ func TestCalculateNextVersion(t *testing.T) {
 
 	for _, test := range tests {
 		actualNewVersion := versioning.CalculateNextVersion(test.currentVersion, test.semanticVersioningTags)
-		assert.Equal(t, test.expectedNewVersion, &actualNewVersion)
+		assert.EqualValues(t, test.expectedNewVersion, &actualNewVersion)
 	}
 }
