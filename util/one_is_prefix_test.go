dc58331a15488fa7ec4209e7d56afbe6da6c29c0
Alexander Kampf <alexander.kampf@thenaitveweb.io>
2022-06-10 13:40:01 +0200

    chore: Add tests for util

---
+++ util/one_is_prefix_test.go
@@ -0,0 +1,30 @@
+package util_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/thenativeweb/getnextversion/util"
+)
+
+func TestOneIsPrefix(t *testing.T) {
+	tests := []struct {
+		string              string
+		possiblePrefixes    []string
+		expectedIsOnePrefix bool
+		expectedPrefix      string
+	}{
+		{string: "foo", possiblePrefixes: []string{}, expectedIsOnePrefix: false, expectedPrefix: ""},
+		{string: "foo", possiblePrefixes: []string{"o"}, expectedIsOnePrefix: false, expectedPrefix: ""},
+		{string: "foo", possiblePrefixes: []string{"f"}, expectedIsOnePrefix: true, expectedPrefix: "f"},
+		{string: "foo", possiblePrefixes: []string{"o", "f"}, expectedIsOnePrefix: true, expectedPrefix: "f"},
+		{string: "foo", possiblePrefixes: []string{"o", "f", "fo"}, expectedIsOnePrefix: true, expectedPrefix: "f"},
+	}
+
+	for _, test := range tests {
+		actual := util.IsOnePrefix(test.string, test.possiblePrefixes)
+
+		assert.Equal(t, test.expectedIsOnePrefix, actual.IsOnePrefix)
+		assert.Equal(t, test.expectedPrefix, actual.Prefix)
+	}
+}
