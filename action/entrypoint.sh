d201447704b6ca9219bef5c9454c59856ae586e9
Alexander Kampf <alexander.kampf@thenaitveweb.io>
2022-07-08 15:44:20 +0200

    Debug

--- action/entrypoint.sh
+++ action/entrypoint.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env sh
 
+echo "[DEBUG] ls -lah ."
+ls -lah .
+
+echo "[DEBUG] ls -lah /github/workspace"
+ls -lah .
+
+echo "[DEBUG] ./get-next-version /github/workspace"
 ./get-next-version /github/workspace
