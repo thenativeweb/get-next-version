0b65ebed7a3a4f5c969f45e9b9b6704b34bc39a9
Alexander Kampf <alexander.kampf@thenaitveweb.io>
2022-07-08 15:48:53 +0200

    chore: Make path in action absolute

--- action/entrypoint.sh
+++ action/entrypoint.sh
@@ -1,10 +1,3 @@
 #!/usr/bin/env sh
 
-echo "[DEBUG] ls -lah ."
-ls -lah .
-
-echo "[DEBUG] ls -lah /github/workspace"
-ls -lah .
-
-echo "[DEBUG] ./get-next-version /github/workspace"
-./get-next-version /github/workspace
+/action/get-next-version /github/workspace
