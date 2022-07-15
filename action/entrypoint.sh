38c875006a2f2b15bbfb57ef0222a4856504de3c
Alexander Kampf <alexander.kampf@thenaitveweb.io>
2022-07-15 15:41:19 +0200

    fix: Add repository flag to make compatible with version command

--- action/entrypoint.sh
+++ action/entrypoint.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-NEXT_VERSION=$(/action/get-next-version /github/workspace)
+NEXT_VERSION=$(/action/get-next-version -r /github/workspace)
 
 git tag "${NEXT_VERSION}"
 git push origin "${NEXT_VERSION}"
