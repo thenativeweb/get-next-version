94c995f139f6aa0e8dcb786575a1fd5b49b7644a
Florian Bauer <florian@fsrv.xyz>
2022-12-03 15:19:30 +0100

    fix: apply suggested review changes
    Signed-off-by: Florian Bauer <florian@fsrv.xyz>

--- cli/root.go
+++ cli/root.go
@@ -15,12 +15,12 @@ import (
 
 var rootRepositoryFlag string
 var rootFormatFlag string
-var rootVersionPrefixFlag string
+var rootPrefixFlag string
 
 func init() {
 	RootCommand.Flags().StringVarP(&rootRepositoryFlag, "repository", "r", ".", "sets the path to the repository")
 	RootCommand.Flags().StringVarP(&rootFormatFlag, "format", "f", "version", "sets the output format")
-	RootCommand.Flags().StringVarP(&rootVersionPrefixFlag, "version-prefix", "p", "", "sets the version prefix")
+	RootCommand.Flags().StringVarP(&rootPrefixFlag, "prefix", "p", "", "sets the version prefix")
 }
 
 var RootCommand = &cobra.Command{
@@ -52,7 +52,7 @@ var RootCommand = &cobra.Command{
 			nextVersion, hasNextVersion = versioning.CalculateNextVersion(result.LatestReleaseVersion, result.ConventionalCommitTypes)
 		}
 
-		lines := cliutil.Format(nextVersion, hasNextVersion, rootFormatFlag, rootVersionPrefixFlag)
+		lines := cliutil.Format(nextVersion, hasNextVersion, rootFormatFlag, rootPrefixFlag)
 		for _, line := range lines {
 			fmt.Println(line)
 		}
--- cliutil/format.go
+++ cliutil/format.go
@@ -2,14 +2,11 @@ package cliutil
 
 import (
 	"fmt"
-	"strings"
-
 	"github.com/Masterminds/semver"
 )
 
-func Format(nextVersion semver.Version, hasNextVersion bool, format string, prefix ...string) []string {
-	prefixString := strings.Join(prefix, "")
-	versionString := prefixString + nextVersion.String()
+func Format(nextVersion semver.Version, hasNextVersion bool, format string, prefix string) []string {
+	versionString := prefix + nextVersion.String()
 
 	switch format {
 	case "github-action":
