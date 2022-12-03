66e3270b9ed086aed347c615c3737a6029338874
Florian Bauer <florian@fsrv.xyz>
2022-12-03 11:32:42 +0100

    feat: add option for version prefixes
    Signed-off-by: Florian Bauer <florian@fsrv.xyz>

--- cliutil/format.go
+++ cliutil/format.go
@@ -2,24 +2,28 @@ package cliutil
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/Masterminds/semver"
 )
 
-func Format(nextVersion semver.Version, hasNextVersion bool, format string) []string {
+func Format(nextVersion semver.Version, hasNextVersion bool, format string, prefix ...string) []string {
+	prefixString := strings.Join(prefix, "")
+	versionString := prefixString + nextVersion.String()
+
 	switch format {
 	case "github-action":
 		return []string{
-			fmt.Sprintf("::set-output name=version::%s", nextVersion.String()),
+			fmt.Sprintf("::set-output name=version::%s", versionString),
 			fmt.Sprintf("::set-output name=hasNextVersion::%v", hasNextVersion),
 		}
 	case "json":
 		return []string{
-			fmt.Sprintf(`{"version": "%s", "hasNextVersion": %v}`, nextVersion.String(), hasNextVersion),
+			fmt.Sprintf(`{"version": "%s", "hasNextVersion": %v}`, versionString, hasNextVersion),
 		}
 	case "version":
 		return []string{
-			nextVersion.String(),
+			versionString,
 		}
 	default:
 		panic("invalid format")
